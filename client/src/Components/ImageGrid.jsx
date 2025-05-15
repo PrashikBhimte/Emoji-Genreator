83f8d41f98f0e4277a0379bc727d27e45ae09a28
PrashikBhimte <prashikbhimte29@gmail.com>
2025-05-15 18:20:29 +0530

    frontend links are updated.

--- client/src/Components/ImageGrid.jsx
+++ client/src/Components/ImageGrid.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 
-export default function ImageGrid(props) {
-
-    const [ grid, setGrid ] = useState(props.grid);
+export default function ImageGrid({grid}) {
 
   return (
     <div className='grid grid-cols-64 w-[640px] h-[640px]' style={{
